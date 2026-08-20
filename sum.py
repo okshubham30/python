a3b1f15b00b366aecdd87f00ae8f644acde69428
okshubham32 <shubham16maury@gmail.com>
2026-08-21 01:56:10 +0530

    this is first practice of python

---
+++ sum.py
@@ -0,0 +1,3 @@
+a = int(input("Entere a number:"))
+b = int(input("Enter second number:"))
+print(a + b)       
---
+++ vote.py
@@ -0,0 +1,5 @@
+n = int(input("Enter your age :"))
+if n >= 18:
+    print("you are eligible for vote")
+else:
+    print("you are not eligible for vote")
